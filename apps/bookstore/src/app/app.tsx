5cd13a99da0da4c93e781b099150759b3626b070
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-10-14 00:33:23 +0700

    add books feature and ui components

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,15 +1,32 @@
-import styled from 'styled-components';
-
-const StyledApp = styled.div``;
+import { Link, Redirect, Route } from 'react-router-dom';
+import { BooksFeature } from '@acme/books/feature';
+// importing the UI library into our App
+import {
+  GlobalStyles,
+  Header,
+  Main,
+  NavigationItem,
+  NavigationList,
+} from '@acme/ui';
 
 export const App = () => {
   return (
-    <StyledApp>
-    <header>
-    <h1>Bookstore</h1>
-    </header>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <Header>
+        <h1>Bookstore</h1>
+        <NavigationList>
+          <NavigationItem>
+            <Link to="/books">Books</Link>
+          </NavigationItem>
+        </NavigationList>
+      </Header>
+      <Main>
+        <Route path="/books" component={BooksFeature} />
+        <Route exact path="/" render={() => <Redirect to="/books" />} />
+      </Main>
+    </>
   );
-}
+};
 
 export default App;
